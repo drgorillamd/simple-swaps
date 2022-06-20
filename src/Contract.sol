16e518b0a625559501969273e23458be9a4068d7
drgorillamd <dr_gorilla@protonmail.com>
2022-06-20 18:30:47 +0200

    pc switch

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
+contract Contract {
+    //todo
+}
